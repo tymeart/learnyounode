a2476805fa01cce461ee16828dbcd8f848ff794a
Tiffany Lam <tlam5@sbcglobal.net>
2017-05-27 14:44:51 -0700

    Redo http collect exercise using bl module

--- httpcollect.js
+++ httpcollect.js
@@ -6,17 +6,13 @@ var url = process.argv[2];
 http.get(url, function(res) {
   res.setEncoding('utf8');
 
-  var allTheData = '';
-  res.on('data', function(chunk) {
-    allTheData += chunk;
-  });
-
-  res.on('end', function() {
-    try {
-      console.log(allTheData.length);
-      console.log(allTheData);
-    } catch (error) {
-      console.log(error.message);
-    }
-  })
+  res.pipe(
+    bl(function(err, data) {
+      if (err) {
+        console.log(err);
+      }
+      console.log(data.toString().length);
+      console.log(data.toString());
+    })
+  );
 });
