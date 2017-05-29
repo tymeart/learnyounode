d0d209bc7e6e5270705aa2b205a83388239ea9e7
Tiffany Lam <tlam5@sbcglobal.net>
2017-05-28 22:54:03 -0700

    Wrap http get request in a function

--- jugglingasync.js
+++ jugglingasync.js
@@ -2,13 +2,23 @@ const http = require('http'),
       bl = require('bl');
 
 var urlsArr = process.argv.slice(2);
+var results = [];
+// number of URLs that returned entire contents
+var count = 0;
 
-for (let i = 0; i < urlsArr.length; i++) {
-  var url = urlsArr[i];
+function getData(index) {
+  var url = urlsArr[index];
   http.get(url, function(res) {
     res.pipe(bl(function(err, data) {
       if (err) console.log(err);
-      console.log(data.toString());
-    }));
+      results[index] = data.toString();
+    }))
   });
+
+  count++;
+}
+
+for (let i = 0; i < urlsArr.length; i++) {
+  getData(i);
+  console.log(results);
 }
